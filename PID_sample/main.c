9549c82424814cd5d00ca3418fca64e5559c72ee
Veli-Matti Kananen <veli-matti.kananen@sensapex.com>
2015-10-21 12:34:54 +0300

    A working and adjusted P-controller (Kp == 1.5)

--- PID_sample/main.c
+++ PID_sample/main.c
@@ -8,10 +8,33 @@
 
 #include <stdio.h>
 #include <math.h> //fabsf
+#include <stdlib.h> //atoi
+
+float Kp = 0;
+float Kd = 0;
+float Ki = 0;
+#define setPoint 100.0
+
+static float PIDcal(float setpoint,float actual_position);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    
+    Kp = atof(argv[1]);
+    Ki = atof(argv[2]);
+    Kd = atof(argv[3]);
+    
+    printf("Kp = %f, Ki = %f, Kd = %f\n", Kp, Ki, Kd);
+
+    float position = 0.21;
+    float control = 0;
+    int adjustCnt = 0;
+    while (fabs(position - setPoint) > 0.1) {
+        control = PIDcal(setPoint, position);
+        position = position + control;
+        adjustCnt++;
+        printf("Adjust# = %d, control = %f, position = %f\n", adjustCnt, control, position);
+    }
     return 0;
 }
 
@@ -21,12 +44,13 @@ int main(int argc, const char * argv[]) {
 #define dt 0.01
 #define MAX 4
 #define MIN -4
-#define Kp 0.1
-#define Kd 0.01
-#define Ki 0.005
+//#define Kp 0.1
+//#define Kd 0.01
+//#define Ki 0.005
 
-float PIDcal(float setpoint,float actual_position)
-{    
+
+float PIDcal(float setpoint, float actual_position)
+{
     static float pre_error = 0;
     static float integral = 0;
     float error;
@@ -51,6 +75,44 @@ float PIDcal(float setpoint,float actual_position)
         output = MAX;
     }
     else if(output < MIN)
+    {
+        output = MIN;
+    }
+    //Update error pre_error = error;
+    return output;
+}
+
+#define mcu_pid_epsilon 0.01
+#define mcu_pid_dt 1/25000*50 // PWM rate 25 kHz, check period 50
+#define mcu_pid_MAX (65355 * 0.05)
+#define mcu_pid_MIN -mcu_pid_MAX
+
+float mcuActuatorPIDdac(float setpoint, float actual_step)
+{
+    static float pre_error = 0;
+    static float integral = 0;
+    float error;
+    float derivative;
+    float output;
+    
+    //Caculate P,I,D
+    error = setpoint - actual_step;
+    
+    //In case of error too small then stop integration
+    if(fabsf(error) > mcu_pid_epsilon)
+    {
+        integral = integral + error*dt;
+    }
+    
+    derivative = (error - pre_error)/dt;
+    output = Kp*error + Ki*integral + Kd*derivative;
+    
+    //Saturation Filter
+    if(output > mcu_pid_MAX)
+    {
+        output = mcu_pid_MAX;
+    }
+    else if(output < mcu_pid_MIN)
     {
         output = MIN;
     }
