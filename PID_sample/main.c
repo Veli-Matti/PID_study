2bbb1dee0983198315edaa6d53d67f8bd1c2e8aa
Veli-Matti Kananen <veli-matti.kananen@sensapex.com>
2015-10-21 08:09:38 +0300

    Initial commit. PIDcal compiles with this version.

--- PID_sample/main.c
+++ PID_sample/main.c
@@ -7,9 +7,53 @@
 //
 
 #include <stdio.h>
+#include <math.h> //fabsf
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
     return 0;
+}
+
+//100ms loop time //ForCurrentSaturation
+
+#define epsilon 0.01
+#define dt 0.01
+#define MAX 4
+#define MIN -4
+#define Kp 0.1
+#define Kd 0.01
+#define Ki 0.005
+
+float PIDcal(float setpoint,float actual_position)
+{    
+    static float pre_error = 0;
+    static float integral = 0;
+    float error;
+    float derivative;
+    float output;
+    
+    //Caculate P,I,D
+    error = setpoint - actual_position;
+    
+    //In case of error too small then stop integration
+    if(fabsf(error) > epsilon)
+    {
+        integral = integral + error*dt;
+    }
+    
+    derivative = (error - pre_error)/dt;
+    output = Kp*error + Ki*integral + Kd*derivative;
+    
+    //Saturation Filter
+    if(output > MAX)
+    {
+        output = MAX;
+    }
+    else if(output < MIN)
+    {
+        output = MIN;
+    }
+    //Update error pre_error = error;
+    return output;
 }
